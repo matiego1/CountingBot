7e31e6ce97d8379666efada56c044a8ecfe06675
Matiego <filipowicz.mati@gmail.com>
2022-11-18 22:27:02 +0100

    added getPosition()

--- src/main/java/me/matiego/counting/UserRanking.java
+++ src/main/java/me/matiego/counting/UserRanking.java
@@ -3,6 +3,7 @@ package me.matiego.counting;
 import me.matiego.counting.utils.Logs;
 import me.matiego.counting.utils.Pair;
 import me.matiego.counting.utils.Response;
+import net.dv8tion.jda.api.entities.UserSnowflake;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
 
@@ -15,10 +16,10 @@ import java.util.List;
 
 public class UserRanking {
 
-    public @NotNull Response add(long user, long guild) {
+    public @NotNull Response add(@NotNull UserSnowflake user, long guild) {
         try (Connection conn = Main.getInstance().getMySQLConnection();
-             PreparedStatement stmt = conn.prepareStatement("INSERT INTO counting_user_ranking(id, guild, amount) VALUES(?, ?, 1) ON DUPLICATE KEY UPDATE amount = amount + 1")) {
-            stmt.setString(1, String.valueOf(user));
+             PreparedStatement stmt = conn.prepareStatement("INSERT INTO counting_user_ranking(id, guild, amount) VALUES(?, ?, 1) ON DUPLICATE KEY UPDATE id = id, guild = guild, amount = amount + 1")) {
+            stmt.setString(1, user.getId());
             stmt.setString(2, String.valueOf(guild));
             stmt.execute();
             return Response.SUCCESS;
@@ -28,10 +29,10 @@ public class UserRanking {
         return Response.FAILURE;
     }
 
-    public @NotNull Response remove(long user, long guild) {
+    public @NotNull Response remove(@NotNull UserSnowflake user, long guild) {
         try (Connection conn = Main.getInstance().getMySQLConnection();
              PreparedStatement stmt = conn.prepareStatement("UPDATE counting_user_ranking SET amount = amount - 1 WHERE id = ? AND guild = ? AND amount > 0;")) {
-            stmt.setString(1, String.valueOf(user));
+            stmt.setString(1, user.getId());
             stmt.setString(2, String.valueOf(guild));
             stmt.execute();
             return Response.SUCCESS;
@@ -41,10 +42,10 @@ public class UserRanking {
         return Response.FAILURE;
     }
 
-    public int get(long user, long guild) {
+    public int get(@NotNull UserSnowflake user, long guild) {
         try (Connection conn = Main.getInstance().getMySQLConnection();
              PreparedStatement stmt = conn.prepareStatement("SELECT amount FROM counting_user_ranking WHERE id = ? AND guild = ?")) {
-            stmt.setString(1, String.valueOf(user));
+            stmt.setString(1, user.getId());
             stmt.setString(2, String.valueOf(guild));
             ResultSet result = stmt.executeQuery();
             if (result.next()) {
@@ -57,8 +58,19 @@ public class UserRanking {
         return -1;
     }
 
-    public int getPosition(long user, long guild) {
-        //TODO: UserRanking#getPosition
+    public int getPosition(@NotNull UserSnowflake user, long guild) {
+        try (Connection conn = Main.getInstance().getMySQLConnection();
+             PreparedStatement stmt = conn.prepareStatement("SELECT 1 + COUNT(*) AS pos FROM counting_user_ranking WHERE amount > (SELECT amount FROM counting_user_ranking WHERE id = ? AND guild = ?)")) {
+            stmt.setString(1, user.getId());
+            stmt.setString(2, String.valueOf(guild));
+            ResultSet result = stmt.executeQuery();
+            if (result.next()) {
+                return result.getInt("pos");
+            }
+            return 0;
+        } catch (SQLException e) {
+            Logs.error("An error occurred while modifying user ranking.", e);
+        }
         return -1;
     }
 
