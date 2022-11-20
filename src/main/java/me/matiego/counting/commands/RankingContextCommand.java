93c04019cb6f7913e8be736d932b99ea2cab49d7
Matiego <filipowicz.mati@gmail.com>
2022-11-20 15:07:42 +0100

    ranking context command

---
+++ src/main/java/me/matiego/counting/commands/RankingContextCommand.java
@@ -0,0 +1,45 @@
+package me.matiego.counting.commands;
+
+import me.matiego.counting.Main;
+import me.matiego.counting.UserRanking;
+import me.matiego.counting.utils.ICommandHandler;
+import me.matiego.counting.utils.Utils;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.context.UserContextInteraction;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class RankingContextCommand implements ICommandHandler {
+    /**
+     * Returns the slash command.
+     *
+     * @return the slash command
+     */
+    @Override
+    public @NotNull CommandData getCommand() {
+        return Commands.user("ranking")
+                .setNameLocalizations(Utils.getAllLocalizations("ranking"))
+                .setGuildOnly(true);
+    }
+
+    @Override
+    public void onUserContextInteraction(@NotNull UserContextInteraction event) {
+        if (!event.getName().equals("ranking")) return;
+        event.deferReply(true).queue();
+
+        User target = event.getTarget();
+        long guild = Objects.requireNonNull(event.getGuild()).getIdLong();
+        UserRanking ranking = Main.getInstance().getUserRanking();
+
+        int pos = ranking.getPosition(target, guild);
+        int amount = ranking.get(target, guild);
+        if (pos <= 0) {
+            event.reply("This user never sent any message, or an error was encountered.").queue();
+            return;
+        }
+        event.reply(target.getAsMention() + " has sent " + amount + "message(s) - " + pos + " place in ranking.").queue();
+    }
+}
